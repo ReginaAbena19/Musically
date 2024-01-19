6f4d6a9a4990e959d47eef3b567dc54b7d4b6be2
Regina <reginaabena.tech@hotmail.com>
2024-01-19 18:26:44 +0000

    [ROB]| complete spotify api controller code complete

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,59 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
   const APIcontroller = function () {
-    const clientId = "";
-    const clientSecret = "";
+      const clientId = "clientID";
+      const clientSecret = "clientSecret";
 
-    const getToken = async () => {
-      const result = await fetch("https://accounts.spotify.com/api/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-wwww-form-urlencoded",
-          Authorization: "Basic " +  btoa(clientId + ":" + clientSecret),
-        },
-        body: "grant_type=client_credentials",
-      }
-      
-      );
-      const data = await result.json();
-      return data.access_token;
-    };
+      const getToken = async () => {
+          const result = await fetch(
+              "https://accounts.spotify.com/api/token",
+              {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/x-www-form-urlencoded",
+                      Authorization:
+                          "Basic " +
+                          btoa(clientId + ":" + clientSecret),
+                  },
+                  body: "grant_type=client_credentials",
+              }
+          );
 
-    const getGenres = async (token) => {
-      const result = await fetch("https://api.spotify.com/v1/browse/categories?locale=sv_UK",
-      {
-        method: "GET",
-        headers: { Authorization: "Bearer " + token},
-      }
-      );
+          const data = await result.json();
 
-      const data = await result.json();
-      return data.categories.items;
-    };
+          return data.access_token;
+      };
 
-    return {getToken, getGenres};
+      const getGenres = async (token) => {
+          const result = await fetch(
+              "https://api.spotify.com/v1/browse/categories?locale=sv_UK",
+              {
+                  method: "GET",
+                  headers: { Authorization: "Bearer " + token },
+              }
+          );
+
+          const data = await result.json();
+          return data.categories.items;
+      };
+
+      return { getToken, getGenres };
   };
 
-  const apiContoller = APIcontroller();
+  const apiController = APIcontroller();
 
   const historyList = $("#history-list");
 
-  function createButton(genreName) {
-    let newBtn = $(`<li class="list-group-item">${genreName}</li>`);
-    historyList.append(newBtn);
+  function createBtn(genreName) {
+      let newBtn = $(`<li class="list-group-item">${genreName}</li>`);
+      historyList.append(newBtn);
   }
 
-  $("#search-button").on("click", async function(event){
-    event.preventDefault();
-    const searchInput = $("#search-input").val().trim();
+  $("#search-button").on("click", async function (event) {
+      event.preventDefault();
+      const searchInput = $("#search-input").val().trim();
 
-    try {
-      const token = await apiContoller.getToken();
-      const genres = await apiContoller.getGenres(token);
-      console.log(genres);
-      createBtn(searchInput);
-    } catch (error) {
-      console.log("Error:", error);
-    }
+      try {
+          const token = await apiController.getToken();
+          const genres = await apiController.getGenres(token);
+          console.log(genres);
+          createBtn(searchInput);
+      } catch (error) {
+          console.error("Error:", error);
+      }
   });
 });
